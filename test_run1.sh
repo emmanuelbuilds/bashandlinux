9eb7a8fec3e9d7bb00dfc7d2704409e1a8f1daf0
emmanuelbuilds <demos.emmanueln@gmail.com>
2024-09-26 23:29:45 -0500

    learning echo and read

---
+++ test_run1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Today is " `date`
+
+echo -e "\nenter the path to directory"
+read the_path
+
+echo -e "\n your path has the following files and folders: "
+ls $the_path
+#Path=/Users/emmanuelnwakaeze/IdeaProjects/bashandlinux
+
+# This is an example comment
+# Both of these lines will be ignored by the interpreter
+
